fd02ad6151be7cc5cbe50fbd43019ffb1aec0739
unknown <andersbundgaard.dk@gmail.com>
2020-12-09 20:09:44 +0100

    Balls with arrays

--- array balls/sketch.js
+++ array balls/sketch.js
@@ -1,25 +1,53 @@
-const ballCount = 100
-let d = new Array(ballCount);
-let x = new Array(ballCount);
-let y = new Array(ballCount);
-
-let xSpeed = new Array (ballCount);
-let ySpeed = new array (ballCount);
+//definitioner på forskellige arrays
+//antal bolde
+const antB =10
+//diameter på bolde
+let ds = new Array(antB);
+//start koordinat til x og y
+let x = new Array(antB);
+let y = new Array(antB);
+//hastighed til x og y 
+let vx = new Array(antB);
+let vy = new Array(antB);
 
 function setup() {
-  createCanvas(400, 400);
-  for (let i = 0; i < ballCount; i++) {
-    d[i] = random(6, 50);
-    x[i] = random(0,width);
-    y[i] = random(0,height);
-  }
-}
+  createCanvas(500, 400);
+//verdier for forskellige arrays
+    for (let i = 0; i < antB; i++) {
+        //random diameter
+        ds[i] = random(10, 20)
+        //spawn location
+        x[i] = random(ds[i], width - ds[i]);
+        y[i] = random(ds[i], height - ds[i]);
+        //hastighed på x og y
+        vx[i] = random(-7,7)
+        vy[i] = random(-7,7)
 
+    }
+}
 function draw() {
-  background(220);
-  for (let i = 0; i < ballCount; i++) {
-    circle(x[i], y[i], d[i]);
-    
-  }
+    background(1);
+    fill(255)
+//tegning af cirkel.
+    for (i = 0; i < antB; i++) {
+//cirkler bliver tegnet
+        circle(x[i], y[i], ds[i]);
+//hastighed til cirkler.
+        x[i] += vx[i];
+        y[i] += vy[i];
+//Borders for balls
+        if (x[i] > width - ds[i]) {
+            vx[i] = -vx[i];
+        }
+        if (x[i] < ds[i]) {
+            vx[i] = -vx[i];
+        }
+        if (y[i] > height - ds[i]) {
+            vy[i] = -vy[i];
+        }
+        if (y[i] < ds[i]) {
+            vy[i] = -vy[i];
+        }
+    }
 }
 
