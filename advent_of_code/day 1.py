a5432abc0f1b410271c14b3e6133606b99367c69
unknown <andersbundgaard.dk@gmail.com>
2020-12-10 09:36:38 +0100

    day one complete

---
+++ advent_of_code/day 1.py
@@ -0,0 +1,34 @@
+lines = [
+]
+
+
+def load_data(fileName):
+    global lines
+    with open(fileName, "r") as input_data:
+        lines = input_data.readlines()
+    for i in range(len(lines)):
+        lines[i] = lines[i].strip()
+
+
+def myHelperFunction():
+    global lines
+    pass
+
+def problemOne():
+    global lines
+
+    for a in lines:
+        for b in lines:
+            x = int(a)
+            y = int(b)
+            if 2020 == x + y:
+                print (x,y,x+y,x*y)
+                return
+
+def problemTwo():
+    pass
+
+if __name__ == "__main__":
+    load_data("day1-input.txt")
+    problemOne()
+    # problemTwo()
