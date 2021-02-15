e1782022a368f85a4ce7d12c49f99165dc713eb3
andersbundgaard <andersbundgaard.dk@gmail.com>
2021-02-15 21:20:07 +0100

    bolde næsten færdige

--- pong_so5/ball.js
+++ pong_so5/ball.js
@@ -1,19 +1,64 @@
-    class bold {
+//ball 1
+class Ball_1 {
     
     //laver positionen for vektoren 
-    constructor(x,y){
-    this.pos = createVector(x, y);
+        constructor(x,y) {
+    
+    //Lav en vektor med koordinater 
+        this.pos_1 = createVector(x, y);
+        
+    // lav en vektor med retnignen den bevæger sig i
+        this.vel_1 = createVector(0,1);
     }
     
     //får bolden til at bevæge sig
-    move (){
-        this.pos.x = this.pos.x + random (-1,1);
-        this.pos.y = this.pos.y + random (-1,1);
+        move_1() {
+        
+    // lægger de to vektorer sammen
+        this.pos_1.add(this.vel_1);
+
+    // // gør det samme som linjen over, bare på en anden måde
+    // this.pos.x = this.pos.x + random (-1,1);        
+    // this.pos.y = this.pos.y + random (-1,1);
     }
 
     //tegner bolden
-    show(){
-        ellipse (this.pos.x, this.pos.y, 40, 40); 
+        show_1(){
+        stroke(255, 100);
+        strokeWeight(2);
+        ellipse (this.pos_1.x, this.pos_1.y, 40, 40); 
     }
-     
 } 
+
+//ball 2
+class Ball_2 {
+    
+    //laver positionen for vektoren 
+        constructor(x,y) {
+    
+    //Lav en vektor med koordinater 
+        this.pos_2 = createVector(x, y);
+        
+    // lav en vektor med retnignen den bevæger sig i
+        this.vel_2 = createVector(0,-1);
+    }
+    
+    //får bolden til at bevæge sig
+        move_2() {
+        
+    // lægger de to vektorer sammen
+        this.pos_2.add(this.vel_2);
+
+    // // gør det samme som linjen over, bare på en anden måde
+    // this.pos.x = this.pos.x + random (-1,1);        
+    // this.pos.y = this.pos.y + random (-1,1);
+    }
+
+    //tegner bolden
+        show_2(){
+        stroke(255, 100);
+        strokeWeight(2);
+        ellipse (this.pos_2.x, this.pos_2.y, 40, 40); 
+    }
+} 
+
--- pong_so5/sketch.js
+++ pong_so5/sketch.js
@@ -1,20 +1,26 @@
 
-let bold;
+let ball_1;
+let ball_2;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  ball = new ball (200, 200);
+  ball_1 = new Ball_1 (width/2, height/2);
+  ball_2 = new Ball_2 (width/2, height/2);
 }
 
 
 function draw() {
   background(40);
   //Linjer som bat er på
-  line (100,0,100,height);
-  line (width-100,0,width-100,height);  
+  line (100, 0, 100, height);
+  line (width-100, 0, width-100, height);  
+  
+  //bold 1 bliver tegnet
+  ball_1.move_1();
+  ball_1.show_1();
 
-  //bolden bliver tegnet
-  ball.move();
-  ball.show();
-
+  
+  //bold 2 bliver tegnet
+  ball_2.move_2();
+  ball_2.show_2();
 }
