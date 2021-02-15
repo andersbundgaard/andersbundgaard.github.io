3752c80c758e37cf8fa1c2083ef1a0de64fb3e22
andersbundgaard <andersbundgaard.dk@gmail.com>
2021-02-15 16:16:16 +0100

    yes

--- pong_so5/sketch.js
+++ pong_so5/sketch.js
@@ -2,12 +2,13 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
+
 function draw() {
   background(40);
-  
   //Linjer som bat er på
   line (100,0,100,windowHeight);
   line (windowWidth-100,0,windowWidth-100,windowHeight);
+  // fill(200)
   
   //Bolden
   
